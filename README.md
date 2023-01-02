c36b7f8e72d4acc7187626405a1677d3fd4e5098
Stanislav Shein <firmvw@gmail.com>
2023-01-02 20:42:20 +0300

    add phpci badge

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/StanislavShein/php-project-9/workflows/hexlet-check/badge.svg)](https://github.com/StanislavShein/php-project-9/actions)
+[![PHP CI](https://github.com/StanislavShein/php-project-9/actions/workflows/phpci.yml/badge.svg)](https://github.com/StanislavShein/php-project-9/actions/workflows/phpci.yml)
--- public/index.php
+++ public/index.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+// Подключение автозагрузки через composer
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome to my site!');
+});
+$app->run();

4ec2fc01da54f2bb319aeef043414eb5ca95fdb6
Stanislav Shein <firmvw@gmail.com>
2023-01-16 13:22:37 +0300

    home page build

--- public/index.php
+++ public/index.php
@@ -4,11 +4,17 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to my site!');
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
 $app->run();

a06cefd197587ed80b33ba1723ca82e81fdd1080
Stanislav Shein <firmvw@gmail.com>
2023-01-21 19:19:30 +0300

    add validator

--- public/index.php
+++ public/index.php
@@ -22,12 +22,26 @@ $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'index.phtml');
 });
 
-$app->post('/urls', function ($request, $response) {
-    $data = $request->getParsedBodyParam('url');
+$app->post('/urls', function ($request, $response) use ($router) {
+    $inputedURL = $request->getParsedBodyParam('url');
+    $url = $inputedURL['name'];
 
-    /*$dsn = "pgsql:host=localhost;port=5432;dbname=hexlet;";
+    /*$parsedURL = parse_url($url);
+    $host = $parsedURL['host'];
+
+    $dsn = "pgsql:host=localhost;port=5432;dbname=hexlet;";
     $pdo = new PDO($dsn, 'stanislav', 'admin', [PDO::ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION]);*/
 
+    $validator = new Valitron\Validator(array('url' => $url));
+    $validator->rule('required', 'url')
+              ->rule('url', 'url')
+              ->rule('lengthMax', 'url', 255);
+
+    if ($validator->validate()) {
+        return $response->write("URL валидный {$url}");
+    } else {
+        return $response->write("URL не валидный {$url}");
+    }
 });
 
 $app->run();

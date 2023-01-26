1a36fc8ed5b2b4227dc3c29f4c1d94014b8edc47
Stanislav Shein <firmvw@gmail.com>
2023-01-26 18:08:24 +0300

    step 4

--- makefile
+++ makefile
@@ -6,6 +6,5 @@ start:
 	sudo service postgresql start
 	PHP_CLI_SERVER_WORKERS=5 php -S 0.0.0.0:$(PORT) -t public
 
-
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 --colors -v public
--- public/index.php
+++ public/index.php
@@ -29,20 +29,81 @@ $app->get('/', function ($request, $response) use ($router) {
     return $this->get('renderer')->render($response, 'index.phtml', $params);
 })->setName('mainpage');
 
+$app->get('/urls', function ($request, $response) use ($router) {
+    $messages = $this->get('flash')->getMessages();
+
+    // подключение к БД
+    $pdo = getConnection();
+
+    // поиск url и даты последней проверки
+    $queryForUrlsAndLastCheck = 'SELECT urls.id AS urls_id, name, MAX(url_checks.created_at) as last_check_time FROM urls
+            LEFT JOIN url_checks ON urls.id = url_checks.url_id
+            GROUP BY urls_id
+            ORDER BY urls_id DESC';
+    $resultOfUrlsAndLastCheck = $pdo->query($queryForUrlsAndLastCheck);
+    $urls = [];
+    foreach ($resultOfUrlsAndLastCheck as $row) {
+        $urls[] = [
+            'id' => $row['urls_id'],
+            'name' => $row['name'],
+            'lastCheckTime' => $row['last_check_time']
+        ];
+    }
+
+    $params = [
+        'flash' => $messages,
+        'urls' => $urls
+    ];
+
+    return $this->get('renderer')->render($response, 'urls.phtml', $params);
+})->setName('urls');
+
+$app->get('/urls/{id}', function ($request, $response, $args) use ($router) {
+    $messages = $this->get('flash')->getMessages();
+
+    // подключение к БД
+    $pdo = getConnection();
+
+    $id = $args['id'];
+
+    // поиск url по id
+    $queryForUrlById = "SELECT * FROM urls WHERE id={$id}";
+    $resultOfUrlById = $pdo->query($queryForUrlById)->fetch();
+    $params = [
+        'flash' => $messages,
+        'url' => [
+            'id' => $id,
+            'name' => $resultOfUrlById['name'],
+            'created_at' => $resultOfUrlById['created_at']
+        ]
+    ];
+
+    // поиск всех проверок url по id
+    $queryForUrlChecks = "SELECT * FROM url_checks WHERE url_id={$id} ORDER BY id DESC";
+    $resultOfUrlChecks = $pdo->query($queryForUrlChecks);
+    $urlChecks = [];
+    foreach ($resultOfUrlChecks as $row) {
+        $urlChecks[] = ['id' => $row['id'], 'created_at' => $row['created_at']];
+    }
+    $params['urlChecks'] = $urlChecks;
+
+    return $this->get('renderer')->render($response, 'url.phtml', $params);
+})->setName('urlId');
+
 $app->post('/urls', function ($request, $response) use ($router) {
+
+    // получение и парсинг url
     $inputtedUrl = $request->getParsedBodyParam('url', null);
     $url = $inputtedUrl['name'];
     $parsedUrl = parse_url($url);
     $scheme = $parsedUrl['scheme'];
     $host = $parsedUrl['host'];
-    $current_time = date('F j, Y \a\t g:ia');
 
     // валидация url
     $validator = new Valitron\Validator(array('url' => $url));
     $validator->rule('required', 'url')
               ->rule('url', 'url')
               ->rule('lengthMax', 'url', 255);
-
     if (!($validator->validate())) {
         $params = [
             'invalidURL' => true,
@@ -52,15 +113,10 @@ $app->post('/urls', function ($request, $response) use ($router) {
     }
 
     // подключение к БД
-    try {
-        $pdo = Connection::get()->connect();
-        $tableCreator = new PostgreSQLCreateTable($pdo);
-        $tables = $tableCreator->createTables();
-    } catch (\PDOException $e) {
-        echo $e->getMessage();
-    }
+    $pdo = getConnection();
 
-    // поиск url в таблице и добавление, если его нет
+    // поиск url в таблице urls, добавление, если его нет и редирект на страницу с url, если есть
+    $current_time = date("Y-m-d H:i:s");
     $queryForCountingUrlsByName = "SELECT COUNT(*) AS counts FROM urls WHERE name='{$scheme}://{$host}'";
     $resultOfCountingUrlsByName = $pdo->query($queryForCountingUrlsByName);
     if (($resultOfCountingUrlsByName->fetch())['counts'] === 0) {
@@ -79,79 +135,32 @@ $app->post('/urls', function ($request, $response) use ($router) {
     return $response->withRedirect($router->urlFor('mainpage'), 302);
 });
 
-$app->get('/urls', function ($request, $response) use ($router) {
-    // подключение к БД
-    try {
-        $pdo = Connection::get()->connect();
-        $tableCreator = new PostgreSQLCreateTable($pdo);
-        $tables = $tableCreator->createTables();
-    } catch (\PDOException $e) {
-        echo $e->getMessage();
-    }
-
-    $sql = 'SELECT id, name FROM urls ORDER BY id DESC';
-    $query = $pdo->query($sql);
-    $urls = [];
-    foreach ($query as $row) {
-        $urls[] = ['id' => $row['id'], 'name' => $row['name']];
-    }
-    $params = [
-        'urls' => $urls
-    ];
-
-    return $this->get('renderer')->render($response, 'urls.phtml', $params);
-})->setName('urls');
-
-$app->get('/urls/{id}', function ($request, $response, $args) use ($router) {
-    // подключение к БД
-    try {
-        $pdo = Connection::get()->connect();
-        $tableCreator = new PostgreSQLCreateTable($pdo);
-        $tables = $tableCreator->createTables();
-    } catch (\PDOException $e) {
-        echo $e->getMessage();
-    }
-
-    $id = $args['id'];
-
-    $queryForUrlById = "SELECT * FROM urls WHERE id={$id}";
-    $urlData = $pdo->query($queryForUrlById)->fetch();
-    $params = [
-        'url' => [
-            'id' => $id,
-            'name' => $urlData['name'],
-            'created_at' => $urlData['created_at']
-        ]
-    ];
-
-    $queryForUrlChecks = "SELECT * FROM url_checks WHERE url_id={$id} ORDER BY id DESC";
-    $resultOfUrlChecks = $pdo->query($queryForUrlChecks);
-    $urlChecks = [];
-    foreach ($resultOfUrlChecks as $row) {
-        $urlChecks[] = ['id' => $row['id'], 'created_at' => $row['created_at']];
-    }
-    $params['urlChecks'] = $urlChecks;
-
-    return $this->get('renderer')->render($response, 'url.phtml', $params);
-})->setName('urlId');
-
 $app->post('/urls/{id}/checks', function ($request, $response, $args) use ($router) {
+
     // подключение к БД
-    try {
-        $pdo = Connection::get()->connect();
-        $tableCreator = new PostgreSQLCreateTable($pdo);
-        $tables = $tableCreator->createTables();
-    } catch (\PDOException $e) {
-        echo $e->getMessage();
-    }
+    $pdo = getConnection();
 
     $id = $args['id'];
 
-    $current_time = date('F j, Y \a\t g:ia');
+    // добавление информации о проверке
+    $current_time = date("Y-m-d H:i:s");
     $queryForNewCheck = "INSERT INTO url_checks (url_id, created_at) VALUES ('{$id}', '{$current_time}')";
     $pdo->query($queryForNewCheck);
+    $this->get('flash')->addMessage('success', 'Страница успешно проверена');
 
     return $response->withRedirect($router->urlFor('urlId', ['id' => $id]), 302);
 });
+
+// функция подключения к БД
+function getConnection() {
+    try {
+        $pdo = Connection::get()->connect();
+        $tableCreator = new PostgreSQLCreateTable($pdo);
+        $tableCreator->createTables();
+        return $pdo;
+    } catch (\PDOException $e) {
+        echo $e->getMessage();
+    }
+}
 
 $app->run();
